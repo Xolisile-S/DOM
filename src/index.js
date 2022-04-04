76fdf9aeb62a3b1f0517ee234505bd8df2b3da56
Xolisile Somtsheu <xolisilesomtsheu@gmail.com>
2022-04-04 06:39:21 +0200

    dom

---
+++ src/index.js
@@ -0,0 +1,32 @@
+import '../assets/styles/style.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import Home from './home';
+import Menu from './menu';
+import Navbar from './navbar';
+
+
+function italicsBody() {
+  document.body.style.fontFamily = "Cambria, Cochin, Georgia, Times, 'Times New Roman', serif";
+}
+italicsBody();
+
+const content = document.getElementById('content');
+const navbar = Navbar();
+content.appendChild(navbar);
+const home = Home();
+content.appendChild(home);
+
+// NAVBAR CLICKING
+
+const menu = Menu();
+
+
+const link1 = document.getElementById('home');
+link1.addEventListener('click', () => {
+  content.replaceChild(home, content.childNodes[1]);
+});
+
+const link2 = document.getElementById('menu');
+link2.addEventListener('click', () => {
+  content.replaceChild(menu, content.childNodes[1]);
+});
